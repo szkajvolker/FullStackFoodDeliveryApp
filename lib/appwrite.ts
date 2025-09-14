aba3b57f275a94eba31950a47b0529aedc79da06
szkajvolker <ist9595@windowslive.com>
2025-09-14 17:41:35 +0200

    finished setup appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteconfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.ist.foodordering",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
